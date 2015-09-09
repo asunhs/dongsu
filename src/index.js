03402bdc4b27228489ae69822b8d844690e81041
Hyunsang Sun <asunhs@gmail.com>
2015-09-09 13:25:03 +0900

    simple layout

--- src/index.js
+++ src/index.js
@@ -3,7 +3,50 @@ import React from 'react';
 
 $(document).ready(function () {
     return React.render(
-        <div>Start</div>,
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Day</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>9/11(Fri)</td>
+                        <td><input type="time"/></td>
+                        <td><input type="time"/></td>
+                        <td><button>Set</button></td>
+                    </tr>
+                    <tr>
+                        <td>9/10(Thr)</td>
+                        <td><input type="time"/></td>
+                        <td><input type="time"/></td>
+                        <td><button>Set</button></td>
+                    </tr>
+                    <tr>
+                        <td>9/9(Wed)</td>
+                        <td><input type="time"/></td>
+                        <td><input type="time"/></td>
+                        <td><button>Set</button></td>
+                    </tr>
+                    <tr>
+                        <td>9/8(Tue)</td>
+                        <td><input type="time"/></td>
+                        <td><input type="time"/></td>
+                        <td><button>Set</button></td>
+                    </tr>
+                    <tr>
+                        <td>9/7(Mon)</td>
+                        <td><input type="time"/></td>
+                        <td><input type="time"/></td>
+                        <td><button>Set</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>,
         document.body
     );
 });
